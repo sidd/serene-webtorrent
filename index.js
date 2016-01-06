c0b7ca55e13878fc68a5f302a556106b8799ff5e
Sidd Sridharan <sidd@sidd.com>
2016-01-06 12:29:04 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var Client = require('./lib/client')
+
+function SereneWebtorrentPlugin (opts) {
+  this._client = new Client(opts)
+}
+
+SereneWebtorrentPlugin.nameIdentifier = SereneWebtorrentPlugin.prototype.nameIdentifier = 'webtorrent'
+SereneWebtorrentPlugin.prettyName = SereneWebtorrentPlugin.prototype.prettyName = 'WebTorrent'
+SereneWebtorrentPlugin.description = SereneWebtorrentPlugin.prototype.description = 'Spawns a WebTorrent instance which is interfaced with'
+SereneWebtorrentPlugin.build = SereneWebtorrentPlugin.prototype.build = SereneWebtorrentPlugin
+
+SereneWebtorrentPlugin.prototype.getTorrents = function () {
+  return {
+    type: 'PROVIDER_TORRENTS',
+    payload: {
+      promise: this._client.getTorrents()
+    }
+  }
+}
+
+SereneWebtorrentPlugin.prototype.addTorrent = function () {
+  return {
+    type: 'PROVIDER_TORRENTS_ADD',
+    payload: {
+      promise: this._client.addTorrent()
+    }
+  }
+}
+
+SereneWebtorrentPlugin.prototype.getStats = function () {
+  return {
+    type: 'PROVIDER_STATS',
+    payload: {
+      promise: Promise.resolve({})
+    }
+  }
+}
+
+module.exports = SereneWebtorrentPlugin
